1b6772c964aa0fdc0196eb2c1a058d4d55103175
Abid <meghpiash@gmail.com>
2023-05-14 18:07:17 +0600

    prompt card and feed is ok

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,7 +1,63 @@
-import React from 'react'
+'use client';
+
+import {BsSearchHeart} from "react-icons/bs"
+import { useState, useEffect } from "react";
+import PromptCard from "./PromptCard";
+
+const PromptCardList = ({data, handleTagClick}) => {
+  return(
+    <div className="mt-16 prompt_layout">
+      {data.map(post=>(
+        <PromptCard 
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  )
+}
 
 export default function Feed() {
+  const [isFocused, setIsFocused] = useState(false);
+  const [posts, setPosts] = useState([]);
+  const [searchText, setSearchText] = useState('');
+  const handleSearchChange = (e) => {
+  }
+  useEffect(()=>{
+    const fetchPost = async () => {
+      const response = await fetch('/api/prompt');
+      const data = await response.json();
+      setPosts(data);
+    }
+    fetchPost();
+  },[]);
   return (
-    <div>Feed</div>
+    <section className="feed">
+      <form className="w-full flex items-center justify-center">
+        <div className={`relative w-full flex items-center bg-black/10 dark:bg-white/10 rounded-full py-2 mx-5
+          ${isFocused?'dark:text-amber-400 ring-1 ring-blue-500 dark:ring-amber-400':'text-white'}`}
+        >
+          <input 
+            type="text"
+            placeholder="Search"
+            value={searchText}
+            onChange={handleSearchChange}
+            onFocus={()=>setIsFocused(true)}
+            onBlur={()=>setIsFocused(false)}
+            required
+            className="w-full py-1 px-5 outline-none bg-transparent"
+          />
+          <span className="absolute right-5">
+            <BsSearchHeart size={25} className={`${isFocused && 'text-blue-500 dark:text-amber-400'}`}/>
+          </span>
+        </div>
+      </form>
+
+      <PromptCardList
+        data={posts}
+        handleTagClick={()=>{}}
+      />
+    </section>
   )
 }
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
+'use client'
 
-export default function PromptCard() {
+import { useState, useEffect } from "react";
+import Image from "next/image";
+import { useSession } from "next-auth/react";
+import { usePathname, useRouter } from "next/navigation";
+
+import {BsHeart, BsHeartFill} from "react-icons/bs"
+
+export default function PromptCard({post, handleTagClick, handleEdit, handleDelete}) {
+  const [heartClicked, setHeartClicked] = useState(false);
   return (
-    <div>PromptCard</div>
+    <div className="prompt_card">
+      <div className="flex justify-between items-start gap-5">
+        <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
+          <Image
+            src={post.creator.image}
+            alt="user_image"
+            width={40}
+            height={40}
+            className="rounded-full object-contain"
+          />
+          <div className="flex flex-col">
+            <h3 className="font-semibold tracking-widest">{post.creator.username}</h3>
+            <p className="text-sm opacity-70">{post.creator.email}</p>
+          </div>
+        </div>
+        <div className="p-2 text-xl cursor-pointer">
+          <div onClick={()=>setHeartClicked((prev)=>!prev)}>
+            {heartClicked?<BsHeartFill className="text-red-500"/>:<BsHeart/>}
+          </div>
+        </div>
+      </div>
+      <p className="my-4">{post.prompt}</p>
+      <p className="text-blue-400 cursor-pointer" onClick={()=>handleTagClick && handleTagClick(post.tag)}>
+        {post.tag}
+      </p>
+    </div>
   )
 }
