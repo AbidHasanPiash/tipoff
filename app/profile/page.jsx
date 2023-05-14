170879c18f9649cfa611160832b627a36a4abe89
Abid <meghpiash@gmail.com>
2023-05-14 22:57:05 +0600

    crud functionality works

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -6,22 +6,36 @@ import { useRouter } from "next/navigation";
 import Profile from "@components/Profile";
 
 export default function MyProfile() {
-    const {data:session} = useSession();
-    const [posts, setPosts] = useState([])
-    useEffect(()=>{
-      const fetchPost = async () => {
-        const response = await fetch(`/api/users/${session?.user.id}/posts`);
-        const data = await response.json();
-        setPosts(data);
-      }
-      if(session?.user.id) fetchPost();
-    },[]);
-    const handleEdid = () => {
+  const router = useRouter();
+  const {data:session} = useSession();
+  const [posts, setPosts] = useState([])
+  useEffect(()=>{
+    const fetchPost = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await response.json();
+      setPosts(data);
+    }
+    if(session?.user.id) fetchPost();
+  },[session?.user.id]);
+  const handleEdid = (post) => {
+    router.push(`/uptade-post?id=${post._id}`)
+  }
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm("Are you sure you want to delete this prompt?");
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: "DELETE",
+        });
 
+        const filteredPosts = posts.filter((item) => item._id !== post._id);
+
+        setPosts(filteredPosts);
+      } catch (error) {
+        console.log(error);
+      }
     }
-    const handleDelete = async () => {
-        
-    }
+  }
   return (
     <Profile
         name="My"
