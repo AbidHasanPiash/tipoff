591f61817041c39cbbaaf1b260fbc9ddacb36eef
Abid <meghpiash@gmail.com>
2023-05-15 20:19:41 +0600

    search functionality added

---
+++ app/profile/[id]/page.jsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
+
+import Profile from "@components/Profile";
+
+export default function UserProfile({ params }) {
+    const searchParams = useSearchParams();
+    const userName = searchParams.get("name");
+  
+    const [userPosts, setUserPosts] = useState([]);
+  
+    useEffect(() => {
+      const fetchPosts = async () => {
+        const response = await fetch(`/api/users/${params?.id}/posts`);
+        const data = await response.json();
+  
+        setUserPosts(data);
+      };
+  
+      if (params?.id) fetchPosts();
+    }, [params.id]);
+  return (
+    <Profile
+      name={userName}
+      desc={`Welcome to ${userName}'s personalized profile page. Explore ${userName}'s exceptional prompts and be inspired by the power of their imagination`}
+      data={userPosts}
+    />
+  )
+}
--- components/Feed.jsx
+++ components/Feed.jsx
@@ -20,18 +20,53 @@ const PromptCardList = ({data, handleTagClick}) => {
 
 export default function Feed() {
   const [isFocused, setIsFocused] = useState(false);
-  const [posts, setPosts] = useState([]);
-  const [searchText, setSearchText] = useState('');
+  const [allPosts, setAllPosts] = useState([]);
+
+  // Search states
+  const [searchText, setSearchText] = useState("");
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchedResults, setSearchedResults] = useState([]);
+
+  const fetchPosts = async () => {
+    const response = await fetch("/api/prompt");
+    const data = await response.json();
+
+    setAllPosts(data);
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const filterPrompts = (searchtext) => {
+    const regex = new RegExp(searchtext, "i"); // 'i' flag for case-insensitive search
+    return allPosts.filter(
+      (item) =>
+        regex.test(item.creator.username) ||
+        regex.test(item.tag) ||
+        regex.test(item.prompt)
+    );
+  };
+
   const handleSearchChange = (e) => {
-  }
-  useEffect(()=>{
-    const fetchPost = async () => {
-      const response = await fetch('/api/prompt');
-      const data = await response.json();
-      setPosts(data);
-    }
-    fetchPost();
-  },[]);
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+
+    // debounce method
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = filterPrompts(e.target.value);
+        setSearchedResults(searchResult);
+      }, 500)
+    );
+  };
+
+  const handleTagClick = (tagName) => {
+    setSearchText(tagName);
+
+    const searchResult = filterPrompts(tagName);
+    setSearchedResults(searchResult);
+  };
   return (
     <section className="feed">
       <form className="w-full max-w-xl flex items-center justify-center">
@@ -55,10 +90,15 @@ export default function Feed() {
       </form>
 
       <div className="px-5">
-        <PromptCardList
-          data={posts}
-          handleTagClick={()=>{}}
-        />
+        {/* All Prompts */}
+        {searchText ? (
+          <PromptCardList
+            data={searchedResults}
+            handleTagClick={handleTagClick}
+          />
+        ) : (
+          <PromptCardList data={allPosts} handleTagClick={handleTagClick} />
+        )}
       </div>
     </section>
   )
